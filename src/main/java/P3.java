aa0db4ca51db11c0a81b2f81a4a590cbcbe22259
Prathap <Prathap@LAPTOP-TI0RNES2>
2022-11-22 12:00:04 +0530

    update

---
+++ src/main/java/P3.java
@@ -0,0 +1,9 @@
+
+public class P3 {
+
+	public static void main(String[] args) {
+		System.out.println("java");
+		System.out.println("selenium");
+		System.out.println("python");
+	}
+}
---
+++ src/main/java/prathap/P2.java
@@ -0,0 +1,10 @@
+package prathap;
+
+public class P2 {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		System.out.println("hello");
+		System.out.println("gm");
+	}
+}
